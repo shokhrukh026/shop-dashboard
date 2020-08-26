f0edf9b1e3d4f2480aa652398663c8c8878fae14
Eldor901 <eldor9012545@gmail.com>
2020-08-26 22:22:22 +0300

    Authorization api and branches is set

--- src/store/user.js
+++ src/store/user.js
@@ -1,54 +1,84 @@
-import axios from 'axios';
+import shop from "../api/shop";
 
-const baseUrl = '/api/v1/user/login/';
-export default{
-    state:{
-        user: {token: ''}
+const baseUrl = "/api/v1/user/login/";
+export default {
+  state: {
+    user: { token: "", user_id: "", business_id: "" },
+  },
+  mutations: {
+    SET_USER_TO_STATE: (state, payload) => {
+      state.user = payload;
     },
-    mutations:{
-        SET_USER_TO_STATE: (state, payload) => {
-            state.user = payload
-        },
-        UNSET_USER_FROM_STATE: (state) => {
-            state.user = {token: ''};
-        },
-        LOG_OUT: (state) =>{
-           sessionStorage.clear();
-        }
+    UNSET_USER_FROM_STATE: (state) => {
+      state.user = { token: "" , user_id: "", business_id: ""};
     },
-    actions: {
-        AUTHORIZATION({commit}, payload) {
-          return axios({
-              method: "POST",
-              url: baseUrl,
-              data: {
-                username: payload.username,
-                password: payload.password
-              },
-            })
-            .then((e) => {
-              commit('SET_USER_TO_STATE', e.data);
-              sessionStorage.setItem('user', JSON.stringify(e.data));
-              payload.router.push({path: '/main'});
-            //   return e;
-            })
-            .catch((error) => {
-              return 'error';
-            })
+    LOG_OUT: (state) => {
+      sessionStorage.clear();
+    },
+  },
+  actions: {
+    /**
+     * @return {string}
+     */
+    async AUTHORIZATION({ commit }, payload) {
+      console.log(payload.username, payload.password);
+      try {
+        const response = await shop.post("user/login/", {
+            username: payload.username,
+            password: payload.password,
+        });
+       //password admin new_admin
 
-          const data = {token: 14212414124412, user_id: 1, business_id: 2};
-          commit('SET_USER_TO_STATE', data);
-          payload.router.push({path: '/main'})
+        commit("SET_USER_TO_STATE", response.data);
+        sessionStorage.setItem("user", JSON.stringify(response.data));
+        payload.router.push({ path: "/main" });
+      } catch (e) {
+        console.log(e + " AUTHORIZATION actions");
+        return "error";
+      }
 
-        },
-        STATE_CHANGED({commit}, payload){
-            commit('SET_USER_TO_STATE', payload);
-        },
-        LOGOUT({commit}){
-          commit('LOG_OUT');
-        }
     },
-    getters:{
-        getUser: state => state.user
+
+    async STATE_CHANGED({commit}, payload){
+      commit('SET_USER_TO_STATE', payload);
+    },
+
+    async LOGOUT({commit}){
+      commit('LOG_OUT');
     }
-}
+
+    // AUTHORIZATION({commit}, payload) {
+    //   return axios({
+    //       method: "POST",
+    //       url: baseUrl,
+    //       data: {
+    //         username: payload.username,
+    //         password: payload.password
+    //       },
+    //     })
+    //     .then((e) => {
+    //       commit('SET_USER_TO_STATE', e.data);
+    //       sessionStorage.setItem('user', JSON.stringify(e.data));
+    //       payload.router.push({path: '/main'});
+    //     //   return e;
+    //     })
+    //     .catch((error) => {
+    //       return 'error';
+    //     })
+    //
+    //   const data = {token: 14212414124412, user_id: 1, business_id: 2};
+    //   commit('SET_USER_TO_STATE', data);
+    //   payload.router.push({path: '/main'})
+    //
+    // },
+    // STATE_CHANGED({commit}, payload){
+    //     commit('SET_USER_TO_STATE', payload);
+    // },
+    // LOGOUT({commit}){
+    //   commit('LOG_OUT');
+    // }
+  },
+  getters: {
+    getUser: (state) => state.user,
+  },
+};
